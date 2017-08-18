b2d2942e47ba524c55588f9444303a1abc65c915
Gilad Shanan <giladshanan@gmail.com>
2017-08-18 16:26:37 -0500

    add name and max speed, enable and disable shield methods

---
+++ spaceship.rb
@@ -0,0 +1,49 @@
+class Spaceship
+  attr_accessor :name
+  attr_reader :max_speed
+
+  def initialize(name, max_speed)
+    @name = name
+    @max_speed = max_speed
+    @shield = false
+  end
+
+  def disable_shield
+    @shield = false
+  end
+
+  def enable_shield
+    @shield = true
+  end
+
+
+end
+
+
+
+# # driver code
+# # Q2
+launch_base = [Spaceship.new("Millenium Falcon", 300000), Spaceship.new("Firefly", 670000)]
+p launch_base
+
+# # Q3
+# launch_base.each do | ship |
+#   p ship.name
+#   p ship.max_speed
+# end
+
+# launch_base[0].name= "Falcon 3000"
+# # this returns an error: launch_base[0].max_speed= "45"
+
+# launch_base.each do | ship |
+#   p ship.name
+#   p ship.max_speed
+# end
+
+# Q4
+launch_base.each do | ship |
+  p ship.enable_shield
+  p ship.disable_shield
+end
+
+
